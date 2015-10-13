2a90e2589f02d8c28272df0648268f46930f0be1
Alexander Kochukov <mail@epsyl.ru>
2015-10-13 14:14:38 +0300

    styles

--- pyha-chrome/src/browser_action/browser_action.js
+++ pyha-chrome/src/browser_action/browser_action.js
@@ -7,15 +7,22 @@ $(function () {
             if (data) {
                 var s_data = $('#data');
                 var s_no = $('#no');
+                var counter = $('#items-new');
                 if (data.length == 0) {
                     s_no.removeClass('hidden');
+                    s_data.addClass('hidden');
                 } else {
                     s_no.addClass('hidden');
+                    s_data.removeClass('hidden');
                     s_data.html('');
+                    counter.hmtl(' (' + data.length + ')');
+
                     for (var t in data) if (data.hasOwnProperty(t)) {
                         s_data.append('<p><a href="#" class="l' + t + '">' + data[t]['name'] + '</a></p>');
                         $('.l' + t).click(function () {
-                            chrome.tabs.create({url: 'http://pyha.ru' + data[t]['url']});
+                            chrome.tabs.create({
+                                url: 'http://pyha.ru' + data[t]['url']
+                            });
                             return false;
                         });
                     }
